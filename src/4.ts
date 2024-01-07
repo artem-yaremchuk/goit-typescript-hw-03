a99fca76441c098792a61573a2a02bbbba99933e
artem-yaremchuk <artem.yaremchuk@yahoo.com>
2024-01-07 15:39:10 +0200

    Feat: Add tasks

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,47 @@
+class Key {
+    private signature: number = Math.random();
+
+    public getSignature(): number {
+        return this.signature;
+    }
+}
+
+class Person {
+    constructor(private key: Key) {}
+
+    public getKey(): Key {
+        return this.key;
+    }
+}
+
+abstract class House {
+    protected door = false;
+    private tenants: Person[] = [];
+
+    constructor(protected key: Key) {}
+
+    public comeIn(person: Person): void {
+        if (this.door) {
+            this.tenants.push(person);
+        }
+    }
+
+    public abstract openDoor(key: Key): boolean;
+}
+
+class MyHouse extends House {
+    constructor(key: Key) {
+        super(key);
+    }
+
+    public openDoor(key: Key) {
+        if (key.getSignature() === this.key.getSignature()) {
+            return this.door = true;
+        }
+        return false;
+    }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
